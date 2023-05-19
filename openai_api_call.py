49156ad7932c911cdf26432f594860a874d7a602
samanthajarrah <samantha.jarrah2@gmail.com>
2023-05-19 11:21:15 -0700

    -made api key hidden -chat completion with user and system roles

--- openai_api_call.py
+++ openai_api_call.py
@@ -1,15 +1,27 @@
 import openai
+import os
 
-openai.api_key = ""
+api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = api_key
 
-completionHistory = openai.ChatCompletion.create(
+grade_level = input("What grade level are your students?: ")
+english_ability = input("What is the English ability?: ")
+question_types = input("What exercise types are you interested in?: ")
+vocab_bank = input("Type out their vocabulary list: ")
+
+completion = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
-    temperature = 1.0,
+    temperature = 0.5,
     max_tokens = 2000,
     messages = [
-    {"role": "user", "content": f"Write {num} multiple choice US history questions, for children {age} years old."}
+    {"role": "user", "content": f"Write multiple English Language exercises for students in grade {grade_level} with {english_ability} English ability. It should include exercises in the following formats: {question_types}. The vocab list is: {vocab_bank}"},
+    {"role": "system", "content": "You are an English Language teacher that creates engaging English Language exercises for your students."}
+
 ]
     )
 
-{completionHistory.choices[0].message.content}
+worksheet = open("english_exercises.txt", "w")
+worksheet.write(f"\n{completion.choices[0].message.content}")
+worksheet.close()
+
 
