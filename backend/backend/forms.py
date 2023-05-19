8d9894ee6771e12ba2c5b568c8aa114fef62955b
Emily <114367783+rave-crave@users.noreply.github.com>
2023-05-19 18:03:08 -0500

    merge to main

--- backend/backend/forms.py
+++ backend/backend/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Selec
 from wtforms.validators import DataRequired
 
 class AppForm(FlaskForm):
-    multiple_choice = BooleanField('Multiple Choice')
+    #multiple_choice = BooleanField('Multiple Choice')
     fill_in_the_blank = BooleanField('Fill in the Blank')
     definition_matching = BooleanField('Definition Matching')
     synonym = BooleanField('Synonym')
@@ -11,5 +11,5 @@ class AppForm(FlaskForm):
     fill_in_the_letters = BooleanField('Fill in the Letters Spelling')
     submit = SubmitField('Get WorkSheet')
     vocabulary_words = StringField('Vocabulary Words', validators=[DataRequired()])
-    grade_level = SelectField('Select Grade Level', choices=[('1', 'Grade Level 1'), ('2', 'Grade Level 2'), ('3', 'Grade Level 3'), ('4', 'Grade Level 4'), ('5', 'Grade Level 5')])
+    #grade_level = SelectField('Select Grade Level', choices=[('1', 'Grade Level 1') ('2', 'Grade Level 2'), ('3', 'Grade Level 3'), ('4', 'Grade Level 4'), ('5', 'Grade Level 5')])
     english_ability = SelectField('Select English Ability', choices=[('1', 'Beginner'), ('2', 'Intermediate'), ('3', 'Advanced')])
