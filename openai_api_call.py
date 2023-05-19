591e57d9a654201e7b480352512195093f9dcb89
samanthajarrah <samantha.jarrah2@gmail.com>
2023-05-19 10:37:13 -0700

    added file

---
+++ openai_api_call.py
@@ -0,0 +1,15 @@
+import openai
+
+openai.api_key = "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"
+
+completionHistory = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    temperature = 1.0,
+    max_tokens = 2000,
+    messages = [
+    {"role": "user", "content": f"Write {num} multiple choice US history questions, for children {age} years old."}
+]
+    )
+
+{completionHistory.choices[0].message.content}
+
